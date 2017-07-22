0541f6fe1e627223a9c57770080d6318a1ca415a
Hayden N <hdnhan@gmail.com>
2017-07-22 22:46:16 +0200

    second update

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+var events = [
+   {name: "Party One", image: ""},
+   {name: "Cultural Event", image: ""},
+   {name: "Trade Show", image: ""},
+   {name: "Osoyoos", image: ""}
+];
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -8,15 +19,22 @@ app.get("/", function(req, res) {
 });
 
 app.get("/events", function(req, res) {
-   var events = [
-      {name: "Party One", image: ""},
-      {name: "Cultural Event", image: ""},
-      {name: "Trade Show", image: ""},
-      {name: "Osoyoos", image: ""}
-   ];
-
    res.render("events", {events:events});
 });
+
+app.get("/events/new", function(req, res) {
+   res.render("new")
+});
+
+app.post("/events", function(req, res) {
+   //get data from form
+   var name = req.body.name;
+   var image = req.body.image;
+   new event = {name: name, image: image}
+   event.push(name, image))
+   res.render("You've hit post in events")
+});
+
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
    console.log("Yelpcamp server started");
